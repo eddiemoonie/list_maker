79c2520254d2cd3fef51cfc6d024a7ad9a444758
Eddie Moon <eddiemoonie@gmail.com>
2018-07-18 19:06:16 +0000

    Application Controller

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "weeklytodolist"
+  end
+
+  get '/' do
+    erb :index
+    "Welcome to Weekly To-Do Calendar"
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+  end
+
+  
