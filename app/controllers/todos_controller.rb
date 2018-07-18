752498574c35d545c3c26376173a05a43f623d62
Eddie Moon <eddiemoonie@gmail.com>
2018-07-18 22:04:56 +0000

    get/post/new todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,29 @@ class TodosController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/todos/new' do
+    if logged_in?
+      erb :'todos/create_todo'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/todos' do
+    if logged_in?
+      if params[:day] == "" || params[:description] == ""
+        redirect "/todos/new"
+      else
+        @todo = current_user.todos.build(:day => params[:day]. :description => params[:description])
+        if @todo.save
+          redirect "/todos/#{@todo.id}"
+        else
+          redirect "/todos/new"
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
