b7c348d0d615f79c3439fa225eb8e3f7129fda1c
Eddie Moon <eddiemoonie@gmail.com>
2018-07-18 21:26:06 +0000

    get/post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = user.id
+      redirect '/todos'
+    end
+  end
+
 end
