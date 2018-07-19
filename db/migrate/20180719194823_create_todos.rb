6c707c9f70cb538d4fc5448e4e0a2f57ede3b015
Eddie Moon <eddiemoonie@gmail.com>
2018-07-19 19:49:36 +0000

    Delete and remake db

--- db/migrate/20180719194823_create_todos.rb
+++ db/migrate/20180719194823_create_todos.rb
@@ -1,6 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
-    create_table :todos do |t|
+    create_table do |t|
       t.string :day
       t.string :description
       t.integer :user_id
