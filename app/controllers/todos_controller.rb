497457e92835cfcb36734a0b8e9b9e8b6bf9d238
Eddie Moon <eddiemoonie@gmail.com>
2018-07-19 19:12:00 +0000

    Edit Task

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -42,4 +42,38 @@ class TodosController < ApplicationController
       redirect '/login'
     end
   end
+
+  get '/todos/:id/edit' do
+    if logged_in?
+      @todo = Todo.find_by_id(params[:id])
+      if @todo && @todo.user == current_user
+        erb :'todos/edit_todo'
+      else
+        redirect '/todos'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/todos/:id' do
+    if logged_in?
+      if params[:description] == ""
+        redirect "/todos/#{params[:id]}/edit"
+      else
+        @todo = Todo.find_by_id(params[:id])
+        if @todo && @todo.user == current_user
+          if @todo.update(:day => params[:day], :description => params[:description])
+            redirect "/todos/#{@todo.id}"
+          else
+            redirect "/tweets/#{@todo.id}/edit"
+          end
+        else
+          redirect '/todos'
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
