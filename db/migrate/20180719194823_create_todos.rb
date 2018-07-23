c26122daa09ac98f45b65845d14a06d3e0b4af90
Eddie Moon <eddiemoonie@gmail.com>
2018-07-23 17:52:19 +0000

    Edit Todos Table

--- db/migrate/20180719194823_create_todos.rb
+++ db/migrate/20180719194823_create_todos.rb
@@ -1,6 +1,6 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
-    create_table do |t|
+    create_table :todos do |t|
       t.string :day
       t.string :description
       t.integer :user_id
