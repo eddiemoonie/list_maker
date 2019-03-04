8671790323f63fbc4263f5593d1f5d7894bc854d
github username <github email address>
2019-03-04 18:24:09 -0500

    create task for list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -43,6 +43,24 @@ class ListsController < ApplicationController
     end
   end
 
+  post '/lists/:id' do
+    if logged_in?
+      @list = List.find_by_id(params[:id])
+      if params[:description] == ""
+        redirect "/lists/#{@list.id}"
+      else
+        @task = @list.tasks.build(:description => params[:description])
+        if @task.save
+          redirect "/lists/#{@list.id}"
+        else
+          redirect "/lists/#{@list.id}"
+        end
+      end
+    else
+      redirect '/login'
+    end
+  end
+
   get '/lists/:id/edit' do
     if logged_in?
       @list = List.find_by_id(params[:id])
